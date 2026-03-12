ba3268cbbc1a53b66b286eb621223894e328e80c
Timur Baiguskarov <timur.baiguskarov@aspose.com>
2026-03-12 11:40:50 +0500

    Release v26.3.0 prep

--- src/Aspose/BarCode/Configuration.php
+++ src/Aspose/BarCode/Configuration.php
@@ -20,7 +20,7 @@ class Configuration implements JsonSerializable
      *
      * @var string
      */
-    protected $clientVersion = '26.2.0';
+    protected $clientVersion = '26.3.0';
 
     /**
      * ClientId for API
