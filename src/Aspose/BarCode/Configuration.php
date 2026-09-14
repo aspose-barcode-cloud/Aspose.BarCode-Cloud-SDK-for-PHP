8301450582ee7ccc3a086b8bbed0e75c77c2839a
TimurBaiguskarovAspose <134321067+TimurBaiguskarovAspose@users.noreply.github.com>
2026-09-14 14:25:45 +0500

    Release v26.9.0 (#110)

--- src/Aspose/BarCode/Configuration.php
+++ src/Aspose/BarCode/Configuration.php
@@ -20,7 +20,7 @@ class Configuration implements JsonSerializable
      *
      * @var string
      */
-    protected $clientVersion = '26.8.0';
+    protected $clientVersion = '26.9.0';
 
     /**
      * ClientId for API
